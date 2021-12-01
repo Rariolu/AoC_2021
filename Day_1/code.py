70aa59da56459660b867f7dac02399704130efab
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-01 18:38:35 +0000

    Implemented part 2 ("sliding windows")

--- Day_1/code.py
+++ Day_1/code.py
@@ -5,6 +5,7 @@ def GetNumbers():
     for i in range(len(l)):
         num = int(l[i])
         nums.append(num)
+    print("len nums",len(nums))
     return nums
 
 def GetIncreases(nums):
@@ -14,4 +15,15 @@ def GetIncreases(nums):
             increases+=1
     return increases
 
-print(GetIncreases(GetNumbers()))
+def GetIncreasesInSlidingWindows(nums):
+    increases = 0
+    for i in range(2, len(nums)-1):
+        win1 = nums[i-2] + nums[i-1] + nums[i]
+        win2 = nums[i-1] + nums[i] + nums[i+1]
+        if win1 < win2:
+            increases+=1
+    return increases
+
+nums = GetNumbers()
+print(GetIncreases(nums))
+print(GetIncreasesInSlidingWindows(nums))
