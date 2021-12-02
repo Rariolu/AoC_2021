16282320c9ab18c74f7c584998d1867ba99b297a
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-02 15:41:10 +0000

    Implemented part 2

--- Day_2/code.js
+++ Day_2/code.js
@@ -41,7 +41,48 @@ function ProcessInstructions(insts)
     return {horizontal: x, depth: y};
 }
 
+function ProcessInstructionsPart2(insts)
+{
+    let x = 0;
+    let y = 0;
+    let dY = 0;
+    for (let i in insts)
+    {
+        let inst = insts[i];
+        let components = inst.split(' ');
+        let operator = components[0];
+        let operand = parseInt(components[1]);
+        switch(operator)
+        {
+            case "forward":
+            {
+                x += operand;
+                y += dY * operand;
+                break;
+            }
+            case "down":
+            {
+                dY += operand;
+                break;
+            }
+            case "up":
+            {
+                dY -= operand;
+                break;
+            }
+        }
+    }
+    return {horizontal:x, depth: y};
+}
+
+console.log("Part 1:");
 let instructions = GetInstructions();
 let finalPosition = ProcessInstructions(instructions);
 console.log(`Final horizontal position is ${finalPosition.horizontal} and final depth is ${finalPosition.depth}.`);
 console.log(`Multiplied is: ${finalPosition.depth * finalPosition.horizontal}.`);
+
+console.log("\n...\nPart 2:");
+
+let finalPosition2 = ProcessInstructionsPart2(instructions);
+console.log(`Final horizontal position is ${finalPosition2.horizontal} and final depth is ${finalPosition2.depth}.`);
+console.log(`Multiplied is: ${finalPosition2.depth * finalPosition2.horizontal}.`);
